a4fe0d9aba08b2ee1d7ed7bb8cc237ed506b5fc6
nadcdc <nadcdc4@gmail.com>
2018-06-10 19:02:57 +0900

    r'^(\d+)/'정규표현식에 해당하는 요청은 post_detail() view function으로 연결시킴    post_detail에서 글 전체 내용을 출력, linebreaksbr사용    post_list와 post_detail에서 Post인스턴스의 text속성값에    linebreaksbr필터를 추가, 줄 바꿈을 보이도록 변경

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from blog.views import post_list
+from blog.views import post_list, post_detail
 
 urlpatterns = [
 
-    url(r'^$' ,post_list)
-
+    url(r'^$' ,post_list),
+    url(r'^(\d+)/',post_detail),
 ]
+
--- app/blog/views.py
+++ app/blog/views.py
@@ -14,3 +14,13 @@ def post_list(request):
 
 
     return render(request,'blog/post_list.html',context)
+
+def post_detail(request, post_id):
+
+    post = Post.objects.get(id = post_id)
+
+    context={
+        'post':post,
+    }
+
+    return render(request, 'blog/post_detail.html',context)
