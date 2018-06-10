354fe75220964ba7ceb4a338c3c31fbf5cf40854
nadcdc <nadcdc4@gmail.com>
2018-06-10 17:04:32 +0900

    post_list에서 blog/post_list.html파일을 동적으로 렌더링    context에 'posts'키로 전달한 QuerySet을 순회하며 div요소를 반복해서 생성해준다.

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Post
 
 
-def post_list(requst):
+def post_list(request):
+
+    posts = Post.objects.all()
+
+    context = {
+        'posts':posts,
+    }
 
 
-    return render(requst,'blog/post_list.html')
+    return render(request,'blog/post_list.html',context)
